a4a07711ec2a48544497268361fe437c73f58310
semicontinuity <igor.a.karpov@gmail.com>
2023-09-09 19:11:09 +0300

    recall folder by frequency

--- fsel/sdk.py
+++ fsel/sdk.py
@@ -150,7 +150,7 @@ class Oracle:
     def memorize(self, path: List[AnyStr], name: AnyStr, persistent: bool):
         pass
 
-    def recall_chosen_name(self, path: Sequence[AnyStr]) -> Optional[AnyStr]:
+    def recall_chosen_name(self, path: List[AnyStr]) -> Optional[AnyStr]:
         pass
 
     def recall_choice(self, path: Sequence[AnyStr], items) -> int:
@@ -169,12 +169,30 @@ class PathOracle(Oracle):
         history[self.string_path(path)] = name
         stat_path = []
         stat_path += path
+        stat_path.append(name)
         stat_path.append('.') # '.' is a special entry for counter
-        self.incr(self.usage_stats if persistent else self.session_stats, stat_path)
+        if persistent:
+            self.incr(self.usage_stats, stat_path)
+        # self.incr(self.usage_stats if persistent else self.session_stats, stat_path)
 
-    def recall_chosen_name(self, path: Sequence[AnyStr]) -> Optional[AnyStr]:
-        string_path = self.string_path(path)
-        return self.visit_history.get(string_path) or self.root_history.get(string_path)
+    def recall_chosen_name(self, path: List[AnyStr]) -> Optional[AnyStr]:
+        # string_path = self.string_path(path)
+        # return self.visit_history.get(string_path) or self.root_history.get(string_path)
+        entry = self.get_entry(self.usage_stats, path)
+        if entry is not None:
+            return self.most_frequent_in(entry)
+        return None
+
+    def most_frequent_in(self, entry: Dict) -> Optional[AnyStr]:
+        top_f = 0
+        result = None
+        for k, v in entry.items():
+            if k != '.':
+                f = v['.']
+                if f > top_f:
+                    top_f = f
+                    result = k
+        return result
 
     def incr(self, stats: Dict, path: List):
         if len(path) == 1:
@@ -187,6 +205,13 @@ class PathOracle(Oracle):
                 stats[path[0]] = entry
 
             self.incr(entry, path[1:])
+
+    def get_entry(self, stats: Dict, path: List):
+        if len(path) == 0:
+            return stats
+        else:
+            entry = stats.get(path[0])
+            return None if entry is None else self.get_entry(entry, path[1:])
 
     @staticmethod
     def string_path(path: Sequence):
