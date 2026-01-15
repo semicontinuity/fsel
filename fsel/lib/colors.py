7c87fda7fe937c2edc50770b101ec1158ba97009
semicontinuity <igor.a.karpov@gmail.com>
2026-01-15 13:47:49 +0300

    Palette cleanup

--- fsel/lib/colors.py
+++ fsel/lib/colors.py
@@ -21,68 +21,10 @@ class Colors:
     B_GREEN = C_B_GREEN
     B_YELLOW = 227
     B_BLUE = C_B_BLUE
-    B_MAGENTA =C_B_MAGENTA
+    B_MAGENTA = C_B_MAGENTA
     B_CYAN = C_B_CYAN
     B_WHITE = C_B_WHITE
 
     C_IDX_BG = 0
     C_IDX_REG_FG = 1
     C_IDX_MATCH_FG = 2
-
-    C_STICKY_FOLDER = [
-        # non focused list; non highlighted entry
-        [BLACK, B_GREEN, B_RED],
-        # non focused list; highlighted entry
-        [BLUE, B_GREEN, B_RED],
-        # focused list; non highlighted entry
-        [BLACK, B_GREEN, B_RED],
-        # focused list; highlighted entry
-        [CYAN, B_GREEN, B_RED]
-    ]
-
-    C_SGID_FOLDER = [
-        # non focused list; non highlighted entry
-        [BLACK, B_YELLOW, B_RED],
-        # non focused list; highlighted entry
-        [BLUE, B_YELLOW, B_RED],
-        # focused list; non highlighted entry
-        [C_BLACK, B_YELLOW, B_RED],
-        # focused list; highlighted entry
-        [CYAN, B_YELLOW, B_RED]
-    ]
-
-    C_SUID_FOLDER = [
-        # non focused list; non highlighted entry
-        [C_BLACK, (192, 48, 48), (255, 64, 64)],
-        # non focused list; highlighted entry
-        [BLUE, (192, 48, 48), (255, 64, 64)],
-        # focused list; non highlighted entry
-        [BLACK, (192, 48, 48), (255, 64, 64)],
-        # focused list; highlighted entry
-        [CYAN, (192, 48, 48), (255, 64, 64)]
-    ]
-
-    C_FOLDER = [
-        # non focused list; non highlighted entry
-        [BLACK, B_WHITE, B_RED],
-        # non focused list; highlighted entry
-        [BLUE, B_WHITE, B_RED],
-        # focused list; non highlighted entry
-        [BLACK, B_WHITE, B_RED],
-        # focused list; highlighted entry
-        [CYAN, B_WHITE, B_RED]
-    ]
-
-    C_LEAF = [
-        # non focused list; non highlighted entry
-        [BLACK, GRAY, B_RED],
-        # non focused list; highlighted entry
-        [BLUE, GRAY, B_RED],
-        # focused list; non highlighted entry
-        [BLACK, GRAY, B_RED],
-        # focused list; highlighted entry
-        [CYAN, BLACK, B_RED]
-    ]
-
-
-colors = Colors()
--- fsel/lib/palette.py
+++ fsel/lib/palette.py
@@ -4,18 +4,75 @@ from .colors import Colors
 from .list_item_info_service import ListItemInfoService
 
 
+class Palette:
+    C_STICKY_FOLDER = [
+        # non focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_GREEN, Colors.B_RED],
+        # non focused list; highlighted entry
+        [Colors.BLUE, Colors.B_GREEN, Colors.B_RED],
+        # focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_GREEN, Colors.B_RED],
+        # focused list; highlighted entry
+        [Colors.CYAN, Colors.B_GREEN, Colors.B_RED]
+    ]
+
+    C_SGID_FOLDER = [
+        # non focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_YELLOW, Colors.B_RED],
+        # non focused list; highlighted entry
+        [Colors.BLUE, Colors.B_YELLOW, Colors.B_RED],
+        # focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_YELLOW, Colors.B_RED],
+        # focused list; highlighted entry
+        [Colors.CYAN, Colors.B_YELLOW, Colors.B_RED]
+    ]
+
+    C_SUID_FOLDER = [
+        # non focused list; non highlighted entry
+        [Colors.BLACK, (192, 48, 48), (255, 64, 64)],
+        # non focused list; highlighted entry
+        [Colors.BLUE, (192, 48, 48), (255, 64, 64)],
+        # focused list; non highlighted entry
+        [Colors.BLACK, (192, 48, 48), (255, 64, 64)],
+        # focused list; highlighted entry
+        [Colors.CYAN, (192, 48, 48), (255, 64, 64)]
+    ]
+
+    C_FOLDER = [
+        # non focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_WHITE, Colors.B_RED],
+        # non focused list; highlighted entry
+        [Colors.BLUE, Colors.B_WHITE, Colors.B_RED],
+        # focused list; non highlighted entry
+        [Colors.BLACK, Colors.B_WHITE, Colors.B_RED],
+        # focused list; highlighted entry
+        [Colors.CYAN, Colors.B_WHITE, Colors.B_RED]
+    ]
+
+    C_LEAF = [
+        # non focused list; non highlighted entry
+        [Colors.BLACK, Colors.GRAY, Colors.B_RED],
+        # non focused list; highlighted entry
+        [Colors.BLUE, Colors.GRAY, Colors.B_RED],
+        # focused list; non highlighted entry
+        [Colors.BLACK, Colors.GRAY, Colors.B_RED],
+        # focused list; highlighted entry
+        [Colors.CYAN, Colors.BLACK, Colors.B_RED]
+    ]
+
+
 def palette(attrs: int, focused_list: bool, focused_entry: bool) -> list[int]:
     """ category: one of C_IDX_* constants """
 
     if (attrs & ListItemInfoService.FLAG_DIRECTORY) and (attrs & S_ISVTX):
-        _palette = Colors.C_STICKY_FOLDER
+        _palette = Palette.C_STICKY_FOLDER
     elif (attrs & ListItemInfoService.FLAG_DIRECTORY) and (attrs & S_ISGID):
-        _palette = Colors.C_SGID_FOLDER
+        _palette = Palette.C_SGID_FOLDER
     elif (attrs & ListItemInfoService.FLAG_DIRECTORY) and (attrs & S_ISUID):
-        _palette = Colors.C_SUID_FOLDER
+        _palette = Palette.C_SUID_FOLDER
     elif attrs & ListItemInfoService.FLAG_DIRECTORY:
-        _palette = Colors.C_FOLDER
+        _palette = Palette.C_FOLDER
     else:
-        _palette = Colors.C_LEAF
+        _palette = Palette.C_LEAF
 
     return _palette[2 * int(focused_list) + int(focused_entry)]
